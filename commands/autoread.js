e96c94e35cea34b0ffd67034a2f7ce0941f9de34
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-10-20 18:25:50 +0300

    Update autoread.js

--- commands/autoread.js
+++ commands/autoread.js
@@ -28,8 +28,8 @@ async function autoreadCommand(sock, chatId, message) {
                     forwardingScore: 1,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+                        newsletterJid: '0029VbAwhrYChq6JPHOMOT0L@newsletter',
+                        newsletterName: 'VAMPARINA Official Channel',
                         serverMessageId: -1
                     }
                 }
@@ -82,8 +82,8 @@ async function autoreadCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029VbAwhrYChq6JPHOMOT0L@newsletter',
+                    newsletterName: 'VAMPARINA Official Channel',
                     serverMessageId: -1
                 }
             }
@@ -97,8 +97,8 @@ async function autoreadCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029VbAwhrYChq6JPHOMOT0L@newsletter',
+                    newsletterName: 'VAMPARINA Official Channel',
                     serverMessageId: -1
                 }
             }
